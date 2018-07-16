7f55a31747996263ae2c7c8638f44b5c8d0c632b
luispmenezes <lspmenezes@gmail.com>
2018-07-16 15:07:38 +0100

    Comita-mos inicial.

---
+++ networkConfigurator.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+if [ `whoami` != root ]; then
+	echo -e '\033[31mSuper user privilages required!\033[0m' 
+	exit $?;
+fi
+
+if ! which curl &> /dev/null ;then
+	echo -e '\033[31mCurl is not installed!!\033[0m'
+    exit $?;
+fi 
+
+if ! which route &> /dev/null ;then
+	echo -e '\033[31mRoute is not installed!!\033[0m'
+    exit $?;
+fi
+
+if ! which ip route &> /dev/null ;then
+	echo -e '\033[31mIp route is not installed!!\033[0m'
+    exit $?;
+fi
+
+ETH_TRGT_W="200"
+WLA_TRGT_W="50"
+LCL_TRGT_W="5"
+
+ETH=$(ip -o link show | awk '{print substr($2, 1, length($2)-1)}' | sed -n 2p)
+WLA=$(ip -o link show | awk '{print substr($2, 1, length($2)-1)}' | sed -n 3p)
+
+ETH_W=$(ip route | awk '$1=="default" {print $5, $9}' | awk -v iface="$ETH" '$1==iface {print $2}')
+WLA_W=$(ip route | awk '$1=="default" {print $5, $9}' | awk -v iface="$WLA" '$1==iface {print $2}')
+
+ETH_GW=$(route -n | awk '$1=="0.0.0.0" {print $8, $2}' | awk -v iface="$ETH" '$1==iface {print $2}')
+WLA_GW=$(route -n | awk '$1=="0.0.0.0" {print $8, $2}' | awk -v iface="$WLA" '$1==iface {print $2}')
+
+echo "Gateways->
+	$ETH: $ETH_GW   metric $ETH_W 
+	$WLA: $WLA_GW   metric $WLA_W"
+
+if [ "$1" = "--revert" ]; then
+	echo "$WLA"
+	echo "Reseting configurations..."
+	ip route del 10.0.0.0/8 via 10.112.82.254
+    export http_proxy="http://194.65.37.122:80"
+    export https_proxy="http://194.65.37.122:80"
+    export ftp_proxy="http://194.65.37.122:80"
+    export no_proxy="localhost,127.0.0.1,.ptin.corppt.com,10.112.97.170,10.112.85.38,ept.telecom.pt,http://asmill01"
+
+	env | grep proxy
+
+	OUT_CONNECT=$(curl -m 20 -s https://google.com/ | wc -l)
+	IN_CONNECT=$(curl -m 20 -s http://maven.ptin.corppt.com/webapp/ | wc -l)
+
+	if [ "$OUT_CONNECT" -gt 500 ] && [ "$IN_CONNECT" -gt 20 ]; then
+		echo  -e '	\033[32mDe-Configuration is OK\033[0m'
+	else
+		echo  -e '	\033[31mDe-Configuration is not OK :(\033[0m'
+	fi  
+
+	exit $?;
+fi 
+
+if [ -z "$ETH_W" ] || [ -z "$WLA_W" ] || [ "$ETH_W" -lt "$WLA_W" ]; then
+	echo "Changing default routes..."
+	ip route del default via $ETH_GW
+	ip route del default via $WLA_GW
+	ip route add default via $ETH_GW dev $ETH proto dhcp metric $ETH_TRGT_W
+	ip route add default via $WLA_GW dev $WLA proto dhcp metric $WLA_TRGT_W
+fi
+
+echo "Setting local routes..."
+
+if ! ip route | grep -q "10.0.0.0/8 via $ETH_GW dev $ETH metric $LCL_TRGT_W"; then
+	echo '	Adding route for Class A'
+	ip route add 10.0.0.0/8 via $ETH_GW dev $ETH metric $LCL_TRGT_W	
+fi
+
+if ! ip route | grep -q " 172.16.0.0/12 via $ETH_GW dev $ETH metric $LCL_TRGT_W"; then
+	echo '	Adding route for Class B'
+	ip route add 172.16.0.0/12 via $ETH_GW dev $ETH metric $LCL_TRGT_W	
+fi
+
+if ! ip route | grep -q "192.168.0.0/16 via $ETH_GW dev $ETH metric $LCL_TRGT_W"; then
+	echo '	Adding route for Class C'
+	ip route add 192.168.0.0/16 via $ETH_GW dev $ETH metric $LCL_TRGT_W	
+fi
+
+echo "Setting DNS..."
+DNS_FILE='/etc/resolv.conf'
+if [ -f /etc/resolvconf/resolv.conf.d/head ]; then
+	DNS_FILE='/etc/resolvconf/resolv.conf.d/head'
+fi
+
+echo 'nameserver 10.112.15.3
+nameserver 10.112.15.6
+nameserver 1.1.1.1
+nameserver 1.0.0.1' > $DNS_FILE
+
+if [ -f /etc/dhcpcd.conf ]; then
+	echo "inteface $ETH
+	metric $ETH_TRGT_W
+
+	interface $WLA
+	metric $WLA_TRGT_W" > /etc/dhcpcd.conf
+fi
+
+grep -q -F '10.112.15.17    asmill01' /etc/hosts || echo '10.112.15.17    asmill01' >> /etc/hosts
+
+echo 'Testing Configuration...'
+
+OUT_CONNECT=$(curl -m 20 -s https://twitter.com/ | wc -l)
+IN_CONNECT=$(curl -m 20 -s http://maven.ptin.corppt.com/webapp/ | wc -l)
+
+if [ "$OUT_CONNECT" -gt 500 ] && [ "$IN_CONNECT" -gt 20 ]; then
+	echo  -e '	\033[32mConfiguration is OK\033[0m'
+else
+	echo  -e '	\033[31mConfiguration is not OK :(\033[0m'
+	ifdown $ETH && ifup $ETH
+	ifdown $WLA && ifup $WLA
+fi  
